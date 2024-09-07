5bc3d09fb7c9c21ead26149447f55003e137f547
VizzWard <vicen102rodr@gmail.com>
2024-09-07 04:36:13 -0600

    Modulo 1

---
+++ 0_ApunteDeCursos/CursoPython/Modulo1/6.operadores.py
@@ -0,0 +1,26 @@
+''' Operadores '''
+# Operadores Relacionales
+
+# Mayor que
+sentencia = 5 > 3
+# Menor que
+sentencia = 5 < 3
+# Mayor o igual
+sentencia = 5 >= 4
+# Menor o igual
+sentencia = 5 <= 4
+# Igual que
+sentencia = 5 == 5
+# Diferente de
+sentencia = 5 != 3
+
+
+# Operadores logicos
+x = 5 ; y = 3
+
+# AND
+resultado = (x == 5) and (y == 3)
+# OR
+resultado = (x < 5) or (y <= 3)
+# NOT
+resultado = not (x < 5)
