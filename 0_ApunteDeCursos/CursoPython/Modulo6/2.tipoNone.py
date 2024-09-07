bd7feff436cae9ee117af055347cf525967ced29
VizzWard <vicen102rodr@gmail.com>
2024-09-07 04:38:38 -0600

    Modulo 6

---
+++ 0_ApunteDeCursos/CursoPython/Modulo6/2.tipoNone.py
@@ -0,0 +1,29 @@
+''' Clase 1 '''
+
+# None sirve para representar la ausencia de un valor
+# Si sabemos que necesitaremos almacenar una variable durante el codigo
+# pero no sabemos que variable tendra al inicio.
+# Podriamos crear una variable inicializada en none y mas adelante almacenar
+# valores en ella
+
+resultado = None
+
+resultado = [1, 2, 3]
+
+print(resultado)
+
+
+''' Clase 2 '''
+
+# Python toma a none como un valor False
+# Los otros valores que python toma como falso son:
+# False
+# None
+# 0
+# 0.0
+# '' -> Cadena vacia
+# / -> Cadena vacia
+# '' -> Cadena vacia
+# [] -> Lista vacia
+# () -> Tupla vacia
+# {} -> Diccionario vacio
---
+++ 0_ApunteDeCursos/CursoPython/Modulo6/6.ternario.py
@@ -0,0 +1,14 @@
+''' Clase 5 '''
+
+# Operador ternario
+x = 5
+resultado = "Es mayor que 3" if x > 3 else "Es menor o igual que 3"
+print(resultado)  # Output: Es mayor que 3
+
+
+''' Clase 6 '''
+
+variable = '' or 'Curso' # Se almacenara el primer valor True
+# En este caso un string vacio se considerario False.
+
+print(variable)
